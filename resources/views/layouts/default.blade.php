17f2a924bad4a8cb4e906ec217928f54922519ea
Shallowpig <s3570641@student.rmit.edu.au>
2018-03-16 21:04:12 +1100

    Nav Bar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,12 @@
 <html>
   <head>
   <title>@yield('title','PRISM') - Pharmancy and Retailer Mactching System</title>
+  <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+        @include('inc.navbar')
+      <div class = "container">
+          @yield('content')
+      </div>
   </body>
 </html>
