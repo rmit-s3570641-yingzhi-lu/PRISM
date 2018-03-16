9b36632ed4e9fb84d9d5e8b22ed5f34797f840b8
Shallowpig <s3570641@student.rmit.edu.au>
2018-03-16 21:19:13 +1100

    main page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -5,7 +5,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
+            <li class="nav-item" id="logo">
               <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
@@ -14,7 +14,11 @@
             <li class="nav-item">
               <a class="nav-link" href="/about">About</a>
             </li>
+            <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+            </li>
           </ul>
+
           <form class="form-inline mt-2 mt-md-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @extends('layouts.default')
 @section('title','HOME')
 @section('content')
-  <h1>home page</h1>
+<div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+          Here is the main page of Laravel
+        </p>
+        <p>
+          Start from here
+        </p>
+        <p>
+          <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+        </p>
+      </div>
 @endsection
