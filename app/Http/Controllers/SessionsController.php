9bd91636b50627800f35bc67e31178fd653367e2
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2018-03-28 12:08:58 +1100

    save changes
    login table will be take off

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,16 +27,17 @@ class SessionsController extends Controller
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
+       
         // User login in info can be remembered for 5 years
        if (Auth::attempt($credentials, $request->has('remember'))) {
-        if(Auth::user()->activated) {
-            session()->flash('success', 'Welcome back!');
-            return redirect()->intended(route('users.show', [Auth::user()]));
-        } else {
-            Auth::logout();
-            session()->flash('warning', 'Your account is not actived, please check your email!');
-            return redirect('/');
-        }
+            if(Auth::user()->activated) {
+                session()->flash('success', 'Welcome back!');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning', 'Your account is not actived, please check your email!');
+                return redirect('/');
+            }
         } else {
             session()->flash('danger', 'Sorry,Wrong Password or Email');
             return redirect()->back();
