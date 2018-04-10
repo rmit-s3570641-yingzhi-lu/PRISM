9bf657852ec4b8108d0aef4155e692a551fef143
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2018-04-10 14:14:38 +1000

    deal with footer issue

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -10,9 +10,31 @@
             <div class="col-md-offset-1 col-md-10">
                 @include('shared._messages')
                 @yield('content')
-                @include('inc._footer')
-      </div>
-      </div>
+            </div>
+      </div>>
+
+      <!--Add by LosoLai 10.04.2018 for fix footer issue-->
+      <footer class="page-footer font-small blue pt-4 mt-4">
+          @include('inc._footer')
+          <!--Footer Links-->
+          <div class="container-fluid text-center text-md-left">
+              <div class="row">
+      
+                  <!--First column-->
+                  <div class="col-md-6">
+                      <h5 class="text-uppercase">Footer Content</h5>
+                      <p>Here you can use rows and columns here to organize your footer content.</p>
+                  </div>
+                  <!--/.First column-->
+      
+          <!--Copyright-->
+          <div class="footer-copyright py-3 text-center">
+              © 2018 Copyright:
+              <a href="https://mdbootstrap.com/material-design-for-bootstrap/"> MDBootstrap.com </a>
+          </div>
+          <!--/.Copyright-->
+      
+      </footer>
       <script src="/js/app.js"></script>
   </body>
 </html>
